28473580fc698ce4d5320fa7365e6d19608412e7
Gyasi Sutton <gyasis@gmail.com>
2022-12-31 09:58:14 -0500

    requirements and spacy model name change

--- augmentation/text_augmentation/augment_textacy.py
+++ augmentation/text_augmentation/augment_textacy.py
@@ -44,7 +44,7 @@ def augment_textacy(textfile, basedir):
   filename1=textfile
   text = open(textfile).read() 
   # "The quick brown fox jumps over the lazy dog."
-  doc = textacy.make_spacy_doc(text, lang="en")
+  doc = textacy.make_spacy_doc(text, lang="en_core_web_sm")
   tfs = [transforms.substitute_word_synonyms, transforms.delete_words, transforms.swap_chars, transforms.delete_chars]
   augmenter=textacy.augmentation.augmenter.Augmenter(tfs, num=[0.5, 0.5, 0.5, 0.5])
   augmented_text=augmenter.apply_transforms(doc)
